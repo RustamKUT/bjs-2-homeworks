9837d250a5a7537b7637a988c204e4a932a33695
Rustam <56912563+RustamKUT@users.noreply.github.com>
2026-01-02 15:55:26 +0500

    task

---
+++ 4.objects/task.js
@@ -0,0 +1,25 @@
+function Student(name, gender, age) {
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+}
+
+Student.prototype.addMarks = function(...marksToAdd) {
+	this.marks ? this.marks.push(...marksToAdd) : null;
+}
+
+Student.prototype.getAverage = function() {
+	return !this.marks || !this.marks.length ? 0 : this.marks.reduce((acc, cur) => acc + cur, 0) / this.marks.length;
+};
+
+
+Student.prototype.exclude = function(reason) {
+	this.subject = undefined;
+	delete this.marks;
+	this.excluded = reason;
+}
