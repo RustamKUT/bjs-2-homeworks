0133c19f9c30269ceba5ca37efaac428f9d35807
Rustam <56912563+RustamKUT@users.noreply.github.com>
2026-01-02 15:11:20 +0500

    task

---
+++ 2.functions/task.js
@@ -0,0 +1,14 @@
+function getArrayParams(...arr) 
+{
+  return { min: min, max: max, avg: avg };
+}
+
+function summElementsWorker(...arr) {}
+
+function differenceMaxMinWorker(...arr) {}
+
+function differenceEvenOddWorker(...arr) {}
+
+function averageEvenElementsWorker(...arr) {}
+
+function makeWork (arrOfArr, func) {}
